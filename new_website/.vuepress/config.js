284c976cdfcc32f11a137312e241403e092654b3
Roshan Dash <roshan@roshandash.com>
2022-02-20 17:46:27 +0530

    Init

--- new_website/.vuepress/config.js
+++ new_website/.vuepress/config.js
@@ -5,7 +5,13 @@ module.exports = {
                 rel: 'icon',
                 href: '/assets/img/logo.png'
             }
-        ]
+        ],
+      ['script', {
+          async: true,
+          src: 'https://easystartup.io/js/analytics.js',
+          'data-api'="https://app.easystartup.io",
+          'data-id'="62123104b47eee4041c48c91"
+      }],
     ],
     markdown: {
         lineNumbers: false // Code block displays line numbers
